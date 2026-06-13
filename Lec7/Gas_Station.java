88cba55bb2d70e1ac423ae638f1eccbf65757951
monukumar98 <monu012as@gmail.com>
2026-06-13 16:43:22 +0530

    Array Leetcode

--- Lec7/Gas_Station.java
+++ Lec7/Gas_Station.java
@@ -9,6 +9,22 @@ public class Gas_Station {
 	}
 
 	public static int Start_index(int[] gas, int[] cost) {
+		int total_sum = 0;
+		for (int i = 0; i < cost.length; i++) {
+			total_sum += gas[i] - cost[i];
+		}
+		if (total_sum < 0) {
+			return -1;
+		}
+		int idx = 0;
+		int sum = 0;
+		for (int i = 0; i < cost.length; i++) {
+			sum += gas[i] - cost[i];
+			if (sum < 0) {
+				idx = i + 1;
+			}
+		}
+		return idx;
 
 	}
 
