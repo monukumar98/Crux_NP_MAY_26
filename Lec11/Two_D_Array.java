979cf62ef3b39656c4e4f9445864bfa32fedbbff
monukumar98 <monu012as@gmail.com>
2026-06-28 15:35:57 +0530

    2D Array

---
+++ Lec11/Two_D_Array.java
@@ -0,0 +1,17 @@
+package Lec11;
+
+public class Two_D_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr = new int[3][4];
+		System.out.println(arr);
+		System.out.println(arr[1]);
+		System.out.println(arr[1][2]);
+		int row = arr.length;// row
+		int col = arr[0].length;// col
+		int[][] other = arr;
+
+	}
+
+}
